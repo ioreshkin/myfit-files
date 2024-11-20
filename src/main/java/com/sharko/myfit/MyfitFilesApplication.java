3b277c8d463475b6c37827f3ebd66b69e1a28b89
PavelSharko <pushkin.94.72@gmail.com>
2024-11-20 16:18:56 +0700

    install commit

---
+++ src/main/java/com/sharko/myfit/MyfitFilesApplication.java
@@ -0,0 +1,13 @@
+package com.sharko.myfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfitFilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyfitFilesApplication.class, args);
+    }
+
+}
