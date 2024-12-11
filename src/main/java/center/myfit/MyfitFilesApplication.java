3bc18e9b98bf954c99d5eed6984e15d832f1948a
sergey-oreshkin <sergey.oreshkin@x5.ru>
2024-12-11 09:38:55 +0300

    поправил пайплайн

--- src/main/java/center/myfit/MyfitFilesApplication.java
+++ src/main/java/center/myfit/MyfitFilesApplication.java
@@ -12,7 +12,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class MyfitFilesApplication {
 
   /**
-   * main.
+   * Запуск приложения.
    */
   public static void main(String[] args) {
     SpringApplication.run(MyfitFilesApplication.class, args);
